f83f66bf7cfadd13f65e2b76f666e0c3ea23d275
Wan Muzaffar <wanmuz86@gmail.com>
2021-12-29 21:41:53 +0800

    add item

--- components/Score.js
+++ components/Score.js
@@ -15,20 +15,66 @@ export default function Score({navigation}) {
       // 1) Get the current high score...
       // 2) If the score > current high score
       // 3) Then only save it as a high score
-      let value =await AsyncStorage.getItem('@high_score')
-      let time = await AsyncStorage.getItem('@time')
+      // let value =await AsyncStorage.getItem('@high_score')
+      // let time = await AsyncStorage.getItem('@time')
       /* THe left part value == null && time == null - for the first try, when nobody played the game */
       /* The right part, after || is when somebody already has a high score.. */
-   
 
-      if (
-        (value == null && time == null || 
-      (navigation.getParam('score') > parseInt(value) && navigation.getParam('duration') < parseFloat(time) ))){
-        await AsyncStorage.setItem('@high_score', navigation.getParam('score'))
-        await AsyncStorage.setItem('@time', navigation.getParam('duration'))
-        setHighScore(true)
+    //   if (
+    //     (value == null && time == null || 
+    //   (navigation.getParam('score') > parseInt(value) && navigation.getParam('duration') < parseFloat(time) ))){
+        
+    //     let newScore = []
+    //     let score = {
+    //       "score":navigation.getParam('score'),
+    //       "duration":navigation.getParam('duration')
+    //     }
+    //     newScore.push(score)
       
+
+    //     await AsyncStorage.setItem('@scores', JSON.stringify(newScore))
+    //     await AsyncStorage.setItem('@high_score', navigation.getParam('score'))
+    //     await AsyncStorage.setItem('@time', navigation.getParam('duration'))
+        
+      
+    //     setHighScore(true)
+      
+    // }
+
+    let highscores = await AsyncStorage.getItem('@scores')
+
+    // THere is no high score in the database
+    console.log(highscores)
+  
+    if (highscores == null){
+      let newScore = []
+      let score = {
+        "score":navigation.getParam('score'),
+        "duration":navigation.getParam('duration')
+      }
+      newScore.push(score)
+    console.log(newScore)
+      await AsyncStorage.setItem('@scores', JSON.stringify(newScore))
     }
+    else if (JSON.parse(highscores).length < 5){
+      let newScore = JSON.parse(highscores)
+      
+      let score = {
+        "score":navigation.getParam('score'),
+        "duration":navigation.getParam('duration')
+      }
+      newScore.push(score) 
+      console.log(newScore)
+      await AsyncStorage.setItem('@scores', JSON.stringify(newScore))
+    }
+
+     
+   
+      setHighScore(true)
+      
+    
+    
+  
     } catch (e) {
       // saving error
     }
